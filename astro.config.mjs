87450e7192a3a0e7e5110abd3d09a49797cf500a
Owen Bower Adams <owen+github@obowersa.net>
2024-03-01 23:07:34 +0000

    add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://obowersa.net',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  redirects: {
+    '/notes': {
+      status: 302,
+      destination: 'https://publish.obsidian.md/obowersa/Notes+Overview'
+    }
+  }
 });
